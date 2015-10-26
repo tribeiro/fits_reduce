465299651b149760f93f7b66b1e73ab68c34f196
William Schoenell <wschoenell@gmail.com>
2015-10-25 22:04:55 -0200

    Added setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='fits_reduce',
+    version='0.1',
+    packages=['fits_reduce', 'fits_reduce.main', 'fits_reduce.tests', 'fits_reduce.util'],
+    url='https://github.com/pablogsal/fits_reduce/',
+    license='BSD',
+    author='Pablo Galindo Salgado',
+    author_email='pablogsal@gmail.com',
+    description='A robotic image reducer for robotic telescopes',
+    install_requires=['astropy',
+                      'colorlog',
+                      'ccdproc>=0.3.3'],
+    scripts=['scripts/reducer']
+)
